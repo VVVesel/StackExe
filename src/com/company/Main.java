b0e4eb35d9d0b17a7d336eb080e0a49bf6cddd65
Vladislav <vladislav.veselinov75@gmail.com>
2022-08-06 12:28:25 +0200

    Example how the stack it works

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	 // Exhample for stack
+        a();
+
+    }
+    public static void a(){
+        System.out.println("a");
+        b();
+
+    }public  static  void b(){
+        System.out.println("B");
+        c();
+    }
+    public static void c(){
+        System.out.println("C");
+    }
+}
